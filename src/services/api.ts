967de23ace759efc3a910ae7fb10caf35a826fd6
Adyson Vieira <contato.adyson@gmail.com>
2024-09-24 19:10:05 -0300

    feat: add getGifts func

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,18 @@
-import { Guest } from '@/app/@types/guest'
+import { Gift } from '@/app/@types/gift'
+import { GuestInputs } from '@/app/confirmacao/schema/confirmationSchema'
 import axios from 'axios'
-import Axios from 'axios'
 
-export const api = Axios.create({
+export const api = axios.create({
   baseURL: process.env.BASE_URL
 })
 
+export const setGuestConfirmation = async (data: GuestInputs) => {
+  const res = await api.post("/api/guest", data)
+  return res.data
+}
 
-
-export const setGuestConfirmation = async (data: Guest) => {
-  const res = await axios.post("http://localhost:5000/api/guest", data)
+export const getGifts = async (): Promise<Gift[]> => {
+  const res = await api.get("/api/gifts")
   return res.data
 }
 
