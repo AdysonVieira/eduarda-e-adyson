1ecb7e0e7f73f7dd29098bcb4f242e9fa69f22e5
Adyson Vieira <contato.adyson@gmail.com>
2024-09-24 19:09:15 -0300

    feat: include escorts input

--- src/app/confirmacao/page.tsx
+++ src/app/confirmacao/page.tsx
@@ -1,24 +1,24 @@
 "use client"
 
-import React, { FormEvent, FormEventHandler } from 'react'
+import React, { FormEvent } from 'react'
 import TextScript from '../_components/TextScript'
+import { setGuestConfirmation } from '@/services/api'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import Flower from '../_components/Flower'
-import { api, setGuestConfirmation } from '@/services/api'
+import { confirmationSchema, GuestInputs } from './schema/confirmationSchema'
+import ErrorMessage from '../_components/ErrorMessage'
 
 const ConfirmationPage = () => {
-  const inputRef = React.useRef<HTMLInputElement | null>(null)
+  const { register, handleSubmit, formState: {errors} } = useForm<GuestInputs>({
+    resolver: yupResolver(confirmationSchema),
+  })
 
-  const submitHandle = async (e: FormEvent) => {
-    e.preventDefault()
-    if (inputRef.current?.value) {
-      const res = await setGuestConfirmation({
-          name: inputRef.current?.value
-        })
-        return res
-    }
-  }
+  const onSubmit: SubmitHandler<GuestInputs> = async (data) => {
+    await setGuestConfirmation(data)
+  } 
 
   return (
     <section className='bg-blue-50 px-5 py-20 w-full flex flex-col gap-10 items-center justify-center relative min-h-screen'>
@@ -40,18 +40,32 @@ const ConfirmationPage = () => {
         </div>
         <p>Aguardamos sua confirmação até o dia 14 de novembro</p>
         <form
-          className='flex flex-col gap-4 w-full'
-          onSubmit={submitHandle}
+          className='flex flex-col gap-6 w-full'
+          onSubmit={handleSubmit(onSubmit)}
         >
-          <input
-            className='border-1 ring-1 border-blue-950 w-full rounded-sm px-2 py-1 text-[1rem]'
-            type='text'
-            name='name'
-            id='name'
-            ref={inputRef}
-            placeholder='Nome completo'
-            autoFocus
-          />
+          <div>
+            <label htmlFor='name' className='block text-start text-[0.75rem]'>Nome</label>
+            <input
+              className='border-1 ring-1 border-blue-950 w-full rounded-sm px-2 py-1 text-[1rem]'
+              {...register("name")}
+              autoFocus
+              placeholder='Digite seu nome completo'
+            />
+            { errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage> }
+          </div>
+
+          <div>
+            <p className='text-start mb-5'>Quantos acompanhantes?</p>
+            <div className='flex justify-between items-center gap-7'>
+              <label htmlFor='name' className='block text-start text-[0.75rem]'>Adultos</label>
+              <input
+                type='number' 
+                className='border-1 ring-1 border-blue-950 rounded-sm px-2 py-1 text-[1rem] w-20'
+                defaultValue={0}
+                {...register("escorts")}
+              />
+            </div>
+          </div>
           <Button className='bg-blue-500 text-blue-100 w-full uppercase' variant="default">confirmar presença</Button>
         </form>
       </div>
