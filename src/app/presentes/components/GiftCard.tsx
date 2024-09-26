41adeef1f51ee391240033eadebb3851cef9f228
Adyson Vieira <contato.adyson@gmail.com>
2024-09-25 22:48:57 -0300

    fix: add button and id

--- src/app/presentes/components/GiftCard.tsx
+++ src/app/presentes/components/GiftCard.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import Image from 'next/image'
+import { formartCurrency } from '@/helpers/formatCurrency';
+import { Button } from '@/components/ui/button';
 
 interface IGiftCardProps {
   name: string;
   price: number;
   imgUrl: string;
+  id: string
 }
 
-const GiftCard = ({name, price, imgUrl}: IGiftCardProps) => {
+const GiftCard = ({name, price, imgUrl, id}: IGiftCardProps) => {
   return (
-    <li className='max-w-48 min-w-40 p-5'>
+    <li className='flex flex-col justify-between items-center gap-4 max-w-48 min-w-36 p-5 bg-blue-100'>
       <Image
-        className='object-cover mb-5'
-          src={imgUrl}
-          alt={name}
-          width={200} 
-          height={200}
+        className='object-cover mb-5 aspect-square'
+        src={imgUrl}
+        alt={name}
+        width={200} 
+        height={200}
         />
       <p>{name}</p>
-      <p>{price}</p>
+      <p>{formartCurrency(price)}</p>
+      <Button className='bg-blue-500 text-blue-100 w-full uppercase' variant="default">presentear</Button>
     </li>
   )
 }
