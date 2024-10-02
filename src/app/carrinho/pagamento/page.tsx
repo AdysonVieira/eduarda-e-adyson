a76c7211294dc836d28d8992182cd335968605b9
Adyson Vieira <contato.adyson@gmail.com>
2024-10-02 18:37:23 -0300

    feat: add inital payment page

---
+++ src/app/carrinho/pagamento/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PaymentDataForm from './components/PaymentDataForm'
+const GuestDataCheckout = () => (
+  <section className='bg-blue-50 px-5 py-20 w-full flex flex-col gap-10 items-center justify-center relative min-h-screen'>
+    <PaymentDataForm />
+  </section>
+)
+
+export default GuestDataCheckout
