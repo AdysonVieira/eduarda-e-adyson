9cec24170679ca1c3bac81d6e0c3a6fc56f41bda
Adyson Vieira <contato.adyson@gmail.com>
2024-10-02 18:35:35 -0300

    fix: change route

--- src/app/presentear/pagamento/schema/GuestDataSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
-export const GuestDataSchema = yup.object({
-  name: yup.string().required('Esse campo é obrigatório'),
-  cpfCnpj: yup.string().required('O campo CPF é obrigatório').transform((value) => value.replace(/[^\d]/g, '')),
-  mobilePhone: yup.string().transform((value) => value.replace(/[^\d]/g, '')),
-})
-
-export type GuestDataInputs = yup.InferType<typeof GuestDataSchema>
