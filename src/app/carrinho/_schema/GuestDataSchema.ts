7e8229d0a632c13c9c15b3f9c2676b63dd1e2b7a
Adyson Vieira <contato.adyson@gmail.com>
2024-10-14 09:29:05 -0300

    feat: add cpf field

--- src/app/carrinho/_schema/GuestDataSchema.ts
+++ src/app/carrinho/_schema/GuestDataSchema.ts
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 
 export const GuestDataSchema = yup.object({
   name: yup.string().required('Esse campo é obrigatório'),
+  cpfCnpj: yup.string().required('O campo CPF é obrigatório').transform((value) => value.replace(/[^\d]/g, '')),
   mobilePhone: yup.string().required('O campo celular é obrigatório').transform((value) => value.replace(/[^\d]/g, '')),
   message: yup.string(),
 })
