cef059899880c6d166a13cd60ce17da795cdcc24
Adyson Vieira <contato.adyson@gmail.com>
2024-10-14 11:10:21 -0300

    feat: add loading and informations

--- src/app/carrinho/_components/GuestDataForm.tsx
+++ src/app/carrinho/_components/GuestDataForm.tsx
@@ -8,18 +8,35 @@ import Button from '@/app/_components/Button'
 import { IMaskInput } from 'react-imask'
 import { useCart } from '@/hooks/useCart'
 import { useRouter } from 'next/navigation'
+import { setCheckout } from '@/services/api'
+import Loading from '@/app/_components/Loading'
 
 const GuestDataForm = () => {
-  const { setGuestData } = useCart()
+  const { setGuestData, gift } = useCart()
+  const [loading, setLoading] = React.useState<boolean>(false)
+  const [error, setError] = React.useState<string | null>(null)
   const router = useRouter()
 
   const { register, control, handleSubmit, formState: { errors } } = useForm<GuestDataInputs>({
     resolver: yupResolver(GuestDataSchema)
   })
-  const onSubmit: SubmitHandler<GuestDataInputs> = (data) => {
-    setGuestData(data)
-    router.push('/carrinho/pagamento')
+  const onSubmit: SubmitHandler<GuestDataInputs> = async (data) => {
+    try {
+      setLoading(true)
+      setGuestData(data)
+      if (gift) {
+        const res = await setCheckout(gift, data);
+        router.push(res.invoiceUrl)
+      }
+    } catch(err) {
+      console.error(err)
+      setError('Erro ao gerar pagamento')
+    } finally {
+      setLoading(false)
+    }
   }
+
+  if(loading) return <Loading message='Gerando cobrança, aguarde.' />
 
   return (
     <div className="w-full text-center flex flex-col items-center gap-5">
@@ -38,6 +55,25 @@ const GuestDataForm = () => {
             placeholder='Nome completo'
           />
           {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+        </div>
+
+        <div className='text-start w-full'>
+          <label htmlFor='cpfCnpj' className='block text-start mb-1'>CPF</label>
+          <Controller
+            name='cpfCnpj'
+            control={control}
+            render={({ field }) => (
+              <IMaskInput
+                {...field}
+                id='cpfCnpj'
+                type='text'
+                className='border border-blue-100 w-full rounded-sm p-2 px-4 text-[1rem]'
+                placeholder='000.000.000-00'
+                mask={"000.000.000-00"}
+              />
+            )}
+          />
+          {errors.cpfCnpj && <ErrorMessage>{errors.cpfCnpj.message}</ErrorMessage>}
         </div>
 
         <div className='text-start'>
@@ -68,6 +104,13 @@ const GuestDataForm = () => {
             rows={5} />
           {errors.message && <ErrorMessage>{errors.message.message}</ErrorMessage>}
         </div>
+
+        <ul className='text-xs text-start flex flex-col gap-3 items-start p-2'>
+          <li><p className='bg-yellow-50'>Para pagamentos do tipo PIX, use preferencialmente o QR Code impresso no convite.</p></li>
+          <li><p>Ao clicar no botão abaixo, você será direcionado para página de pagamento.</p></li>
+          <li><p>Em caso de problemas durante o direcionamento para o pagamento, use o QR Code impresso no convite para concluir o pagamento.</p></li>
+        </ul>
+
         <Button>ir para pagamento</Button>
       </form>
     </div>
