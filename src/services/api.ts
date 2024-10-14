83ca7ce0f04a5319650a72347ad06cc603334a9f
Adyson Vieira <contato.adyson@gmail.com>
2024-10-14 09:29:43 -0300

    fix: remove unnecessary imports

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios'
-import { GiftData, GuestData } from '@/app/_types/checkout'
-import { PaymentGuestDataInputs } from '@/app/carrinho/pagamento/_schema/PaymentGuestDataSchema'
-import { PaymentCardDataInputs } from '@/app/carrinho/pagamento/_schema/PaymentCardDataSchema'
-import { PixQrCodeResponse } from './CheckoutPixService'
 import { GuestDataInputs } from '@/app/carrinho/_schema/GuestDataSchema'
-import { CreditCardResponse } from './CheckoutCreditCardService'
+import { Gift } from '@prisma/client'
+import { CheckoutResponse } from './CheckoutService'
 
 export const api = axios.create({
   baseURL: process.env.BASE_URL
@@ -17,20 +14,11 @@ export const apiAsaas = axios.create({
   }
 });
 
-export const setCreditCardCheckout = async (gift: GiftData, guest: GuestDataInputs, paymentGuestData: PaymentGuestDataInputs, paymentCardData: PaymentCardDataInputs,): Promise<CreditCardResponse> => {
 
+export const setCheckout = async (gift: Gift, guest: GuestDataInputs)=> {
   const res = await api.post("/api/checkout", {
     gift,
     guest,
-    paymentGuestData,
-    paymentCardData,
   })
   return res.data
 };
-
-export const setPixCheckout = async (gift: GiftData, guest: GuestDataInputs): Promise<PixQrCodeResponse> => {
-  const res = await api.post("/api/pix", { gift, guest })
-  return res.data
-};
-
-
