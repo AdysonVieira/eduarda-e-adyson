4e5037cdea1be82f326abb375013001d72ce9e54
Adyson Vieira <contato.adyson@gmail.com>
2024-10-14 11:13:33 -0300

    fix: remove unnecessary states

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { GuestDataInputs } from "@/app/carrinho/_schema/GuestDataSchema";
-import { PaymentCardDataInputs } from "@/app/carrinho/pagamento/_schema/PaymentCardDataSchema";
-import { PaymentGuestDataInputs } from "@/app/carrinho/pagamento/_schema/PaymentGuestDataSchema";
 import { Gift } from "@prisma/client";
 import React, { ReactNode, SetStateAction } from "react";
 
@@ -18,10 +16,6 @@ interface CartContextProps {
   setGuestData: React.Dispatch<SetStateAction<GuestDataInputs | undefined>>;
   guestConfirmation: GuestConfirmation | undefined | null
   setGuestConfirmation: React.Dispatch<SetStateAction<GuestConfirmation | undefined>>;
-  paymentGuestData: PaymentGuestDataInputs | undefined
-  setPaymentGuestData: React.Dispatch<SetStateAction<PaymentGuestDataInputs | undefined>>;
-  paymentCardData: PaymentCardDataInputs | undefined
-  setPaymentCardData: React.Dispatch<SetStateAction<PaymentCardDataInputs | undefined>>;
   resetValues: () => void;
 }
 
@@ -34,14 +28,11 @@ const CartProvider = ({ children }: CartProviderProps) => {
   const [guestData, setGuestData] = React.useState<GuestDataInputs>()
   const [guestConfirmation, setGuestConfirmation] = React.useState<GuestConfirmation>()
   const [gift, setGift] = React.useState<Gift>()
-  const [paymentGuestData, setPaymentGuestData] = React.useState<PaymentGuestDataInputs>()
-  const [paymentCardData, setPaymentCardData] = React.useState<PaymentCardDataInputs>()
+
 
   const resetValues = React.useCallback(() => {
     setGift(undefined)
     setGuestData(undefined)
-    setPaymentGuestData(undefined)
-    setPaymentCardData(undefined)
   }, [])
   return (
     <CartContext.Provider value={{
@@ -51,10 +42,6 @@ const CartProvider = ({ children }: CartProviderProps) => {
       setGuestData,
       guestConfirmation,
       setGuestConfirmation,
-      paymentGuestData,
-      setPaymentGuestData,
-      paymentCardData,
-      setPaymentCardData,
       resetValues,
     }}>
       {children}
